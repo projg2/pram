096e89a56d12bb468f91e9264042fbbf60e68f58
Vivian Heisz (demize) <demize@unstable.systems>
2025-04-21 12:52:41 -0400

    Add `-p/--part-of` option to add a trailer linking the PR
    This is #9 revived and tweaked a little.
    
    Most of the review feedback should be addressed, and along
    with it, things should be optimized a little more. The most
    notable change (both in terms of simplified code and in
    terms of behavior) is that `linked_pr` is gone, replaced by
    `to_close`, per one of the review comments. This has the
    effect of adding `Closes:` trailers in some new places,
    which I think is generally desirable, as it allows you to
    close a pull request while applying it as a patch file or
    from an unrelated URL.
    
    Following on from that, `--part-of-pr` has been renamed to
    `--link-to`. I've also dropped the `-p` implication here,
    as it might be useful for overriding the final `Closes:`
    even if the `Part-of:` trailer is unwanted. I've kept the
    restriction on only providing one, since I can't see any
    justification for more than one here (nor do I understand
    how it would -work-).
    
    All tests are also updated to pass with the new trailer,
    mostly by disabling it, but a couple pass `--link-to 123`.
    
    Signed-off-by: Vivian Heisz (demize) <demize@unstable.systems>
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- test/11combined-partof-signoff-bug.sh
+++ test/11combined-partof-signoff-bug.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Test whether applying a plain patch via pram works.
+# Test whether Part-of is properly combined with signoff and bug/closes tags.
+# Minor variation on 03combined-signoff-bug.
 
 set -e -x
 
@@ -7,10 +8,11 @@ set -e -x
 
 cat > trivial.patch <<-EOF
 	From 88460bc61f56546da478dc6fd4682e7c62cc6c80 Mon Sep 17 00:00:00 2001
-	From: PRam test <pram@example.com>
+	From: Other person <other@example.com>
 	Date: Sat, 1 Jan 2000 00:00:00 +0000
 	Subject: [PATCH] A trivial patch
 
+	Signed-off-by: Other person <other@example.com>
 	---
 	 data.txt    | 4 ++--
 	 newfile.txt | 1 +
@@ -38,25 +40,31 @@ cat > trivial.patch <<-EOF
 	--- /dev/null
 	+++ b/newfile.txt
 	@@ -0,0 +1 @@
-	+Also, a new file with CRLF line ending.
+	+Also, a new file.
 	--
 	2.21.0
 EOF
 
-bash "${INITDIR}"/../pram --no-gitconfig -e true -G -I -S --link-to 123 ./trivial.patch
+bash "${INITDIR}"/../pram --no-gitconfig -e true -G -I -s -b 314152 -c 314154 --link-to 123 ./trivial.patch
 
-git log --format='%ae%n%an%n%aI%n%B' -1 > git-log.txt
+git log --format='%ae%n%an%n%ce%n%cn%n%aI%n%B' -1 > git-log.txt
 diff -u - git-log.txt <<-EOF
+	other@example.com
+	Other person
 	pram@example.com
 	PRam test
 	2000-01-01T00:00:00Z
 	A trivial patch
 
+	Signed-off-by: Other person <other@example.com>
 	Part-of: https://github.com/gentoo/gentoo/pull/123
+	Bug: https://bugs.gentoo.org/314152
+	Closes: https://bugs.gentoo.org/314154
 	Closes: https://github.com/gentoo/gentoo/pull/123
+	Signed-off-by: PRam test <pram@example.com>
 
 EOF
 sha1sum -c <<EOF
 8054584c7b1fa9b5bdd7ee1177e78c99ea2cce04  data.txt
-f83370dac2432114808711e29f399f9b0c87fc23  newfile.txt
+810ded956f70861874e2c6083c5dc9e9e80f1808  newfile.txt
 EOF
